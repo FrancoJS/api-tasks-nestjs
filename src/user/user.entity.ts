1b65222a131db2fc9bc812f6fe499e8898f9289a
Franco_SJ <jean.jara.fs@gmail.com>
2025-07-29 20:53:22 -0400

    feat: agregue metodo de creacion de usuarios

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -15,6 +15,7 @@ export class User {
 
   @Column({
     type: 'varchar',
+    length: 50,
     unique: true,
     nullable: false,
   })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
+
 @Injectable()
 export class UserService {
   constructor(
@@ -10,8 +15,33 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  createUser(user: CreateUserDto): string {
-    this.userRepository.save(user);
-    return 'El usuario ha sido creado';
+  public async createUser(user: CreateUserDto) {
+    try {
+      const userExists = await this.userExists(user.username, user.email);
+      if (userExists) {
+        throw new ConflictException('El usuario ya existe');
+      }
+      await this.userRepository.save(user);
+      return 'El usuario ha sido creado';
+    } catch (error) {
+      if (error instanceof ConflictException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException(
+        'No se pudo conectar con la base de datos',
+        {
+          description: 'Hubo un error en la conexion',
+        },
+      );
+    }
+  }
+
+  public async userExists(username: string, email: string) {
+    const user = await this.userRepository.findOne({
+      where: [{ username }, { email }],
+    });
+
+    return !!user;
   }
 }
